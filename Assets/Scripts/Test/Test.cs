6846fbb6c0d89a9b2cae2fdeee3e17fbb9223c8f
lllwk <admin>
2024-05-28 19:55:41 +0800

    测试提交

--- Assets/Scripts/Test/Test.cs
+++ Assets/Scripts/Test/Test.cs
@@ -1,12 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class MyCollection:IEnumerable<int>
+{
+    private int[] _items;
+    public MyCollection(int[] _items) 
+    {
+        this._items = _items;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new MyEnumerator(_items);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class MyEnumerator : IEnumerator<int>
+{
+    private int[] _items;
+    private int _postion = -1;
+
+    public MyEnumerator(int[] _items)
+    {
+        this._items = _items;
+    }
+
+    public int Current
+    {
+        get
+        {
+            if(_postion<0||_postion>=_items.Length)
+            {
+                throw new InvalidOperationException();
+            }
+            return _items[_postion];
+        } 
+    }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+        
+    }
+
+    public bool MoveNext()
+    {
+        _postion++;
+        return _postion < _items.Length;
+    }
+
+    public void Reset()
+    {
+        _postion = -1;
+    }
+}
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        var items = new int[] { 1, 2, 3, 4, 5 };
+        var collection=new MyCollection(items);
+        foreach (var item in collection)
+        {
+            Debug.Log(item);
+        }
         Debug.Log("更改提交");
     }
 
