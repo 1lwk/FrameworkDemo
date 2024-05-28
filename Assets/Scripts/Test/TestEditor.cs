751f6d88154f493b5f601846fd7e2d95ffd22097
lllwk <admin>
2024-05-28 10:12:18 +0800

    修改测试提交

--- Assets/Scripts/Test/TestEditor.cs
+++ Assets/Scripts/Test/TestEditor.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class TestEditor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("更改提交");
+        
     }
 
     // Update is called once per frame
